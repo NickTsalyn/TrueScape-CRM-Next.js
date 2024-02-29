4d258a3ed44975099626e783b110b6191a1b08ce
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-02-29 14:55:24 -0500

    add companies, dashboard routes
    add all modals, and dynamic routes

---
+++ crm/src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Header from '@/app/components/header';
+
+
+
+
+export default function Page () {
+  return (
+    <>
+      <Header>Dashboard</Header>
+    </>
+  );
+}
---
+++ crm/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+     <h1 className="text-xl">Home Page</h1>
+    </main>
+  );
+}
