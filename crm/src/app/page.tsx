41682e1525f504081547979af04314fcb4e05de5
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-02-29 16:46:20 -0500

    update HomePage

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
 export default function Home() {
   return (
-    <main>
-     <h1 className="text-xl">Home Page</h1>
+    <main className="flex items-center justify-center h-screen w-screen bg-gradient-to-br from-green-200 to-purple-200 p-8 rounded">
+      <div className="">
+        <Link
+          href="/dashboard"
+          className="bg-gray-900 py-2 px-4 rounded text-neutral-50 font-semibold hover:bg-gradient-to-br from-green-200 to-purple-200 hover:text-gray-900 transition duration-300 text-2xl"
+        >
+          Get Started
+        </Link>
+      </div>
     </main>
   );
 }
